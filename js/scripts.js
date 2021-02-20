690cad21a689e5c45d74e77e13e8c4f0c43fe765
Maggie S <Maggiesaldivia@gmail.com>
2021-02-20 13:33:19 -0800

    add score prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,35 @@
-//arrays
-let playersArray = []
-
-
+//business logic for dice
 function Dice() {
-  this.diceValue = 1;
+	this.diceValue=1;
+  this.roll=0;
 }
 
 function Player(name) {
 	this.name = name;
   this.turnScore = 0;
   this.totalScore = 0;
-  this.dice = new Dice;
-  playersArray.push(this);
+  this.dice = new Dice();
+  this.dice.rollDice()
 
 }
 
 Dice.prototype.rollDice = function() {
   this.diceValue = Math.floor((Math.random()*6)+1);
   if (this.diceValue === 1) {
-    this.turnScore = 0;
-    return "turn score: " + this.turnScore
+    this.roll = 0;
+    return "current roll: " + this.roll
   } else {
-  	this.turnScore = this.diceValue;
-  	return "turn score: " + this.turnScore
+  	this.roll = this.diceValue;
+  	return "current roll: " + this.roll
 	}
+}
+
+Player.prototype.score = function() {
+  if (this.dice.roll === 0) {
+    this.turnScore = 0;
+    return this.turnScore
+  } else {
+  	this.turnScore += this.dice.roll
+    return this.turnScore
+  }
 }

cafca247665b41d4c8a3f9b33b0809a7feaa0bae
Maggie S <Maggiesaldivia@gmail.com>
2021-02-21 14:43:14 -0800

    fix bug and now both players switch points succesfully

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-
-
 //business logic for dice
 function Dice() {
 	this.diceValue=1;
@@ -9,27 +7,35 @@ function Dice() {
 function Player() {
   this.score = 0;
   this.dice = new Dice()
-  this.isTurn = 'yes'
+  this.isTurn = true
 }
 //global Players
 player1 = new Player();
 player2 = new Player();
 
+function switchPlayers() {
+  if (player1.isTurn === true) {
+    player1.isTurn = false;
+    player2.isTurn = true;
+  } else if (player2.isTurn === true) {
+    player2.isTurn = false;
+    player1.isTurn = true;
+  }
+}
+
 Dice.prototype.rollDice = function() {
   this.diceValue = Math.floor((Math.random()*6)+1);
   if (this.diceValue === 1) {
     this.roll = 0;
-    return "current roll: " + this.roll
   } else {
   	this.roll = this.diceValue;
-  	return "current roll: " + this.roll
 	}
 }
 
 Player.prototype.calcScore = function() {
 	this.dice.rollDice()
   if (this.dice.roll === 0) {
-    this.isTurn = 'no'
+    switchPlayers();
   } else {
   	this.score += this.dice.roll
   }
@@ -38,11 +44,19 @@ Player.prototype.calcScore = function() {
 
 
 
+
+
 //ui logic 
 $(document).ready(function() {
 	$("button.btn-roll").click(function(event) {
-  	player1.calcScore();
-    $('#p1-total').html(player1.score);
-    $('#p1-roll').html(player1.dice.roll);
-  })
-})
+  	if (player1.isTurn !== false) {
+      player1.calcScore();
+      $('#p1-total').html(player1.score);
+      $('#p1-roll').html(player1.dice.roll);
+    } else if (player2.isTurn === true) {
+      player2.calcScore();
+      $('#p2-total').html(player2.score);
+      $('#p2-roll').html(player2.dice.roll)
+    }
+    });
+    });
